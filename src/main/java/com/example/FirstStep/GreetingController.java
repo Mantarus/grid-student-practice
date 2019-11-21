752dff675cb45d17e389249d7c7cbc2c68f2cb5b
l4v7l <vovan_lipkin@mail.ru>
2019-11-22 00:07:36 +0300

    Initial commit, как говорится

---
+++ src/main/java/com/example/FirstStep/GreetingController.java
@@ -0,0 +1,25 @@
+package com.example.FirstStep;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
+        model.put("name", name);
+        return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model) {
+        model.put("some", "Wow, it's amazing");
+        return "main";
+    }
+
+}
