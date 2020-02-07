7ebd1265d6ef825e57a54e9e0c9806bb3674272f
Vova <vovan_lipkin@mail.ru>
2020-02-07 13:32:50 +0300

    DB connected

--- backend/src/main/java/com/gridstudentpractice/chatservice/Application.java
+++ backend/src/main/java/com/gridstudentpractice/chatservice/Application.java
@@ -4,11 +4,45 @@ package com.gridstudentpractice.chatservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+    static final String DB_URL = "jdbc:postgresql://localhost:5432/chat";
+    static final String USER = "postgres";
+    static final String PASS = "password";
 
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("PostgreSQL JDBC Driver is not found. Include it in your library path ");
+            return ;
+        }
+
+        System.out.println("PostgreSQL JDBC Driver successfully connected");
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection Failed");
+            return ;
+        }
+
+        if (connection != null) {
+            System.out.println("You successfully connected to database now");
+        } else {
+            System.out.println("Failed to make connection to database");
+        }
+    }
 }
---
+++ backend/src/main/java/com/gridstudentpractice/chatservice/message/service/DBMessageServiceImpl.java
@@ -0,0 +1,25 @@
+package com.gridstudentpractice.chatservice.message.service;
+
+import com.gridstudentpractice.chatservice.message.model.Message;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+/* public class DBMessageServiceImpl implements MessageService {
+
+    @Override
+    public Message sendMessage(Message message) {
+        return null;
+    }
+
+    @Override
+    public List<Message> getMessages() {
+        return null;
+    }
+} */
+
+public class DBMessageServiceImpl {
+
+
+}
