52fa5d41c00dc8ffa34cad8f71376836a29a5bc9
Trofim <ktrof18@gmail.com>
2020-03-07 20:27:34 +0300

    Separate class created for Swagger configuration

--- backend/src/main/java/com/gridstudentpractice/chatservice/Application.java
+++ backend/src/main/java/com/gridstudentpractice/chatservice/Application.java
@@ -1,46 +1,27 @@
 package com.gridstudentpractice.chatservice;
 
+import com.gridstudentpractice.chatservice.config.SwaggerConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
 public class Application {
+
+    @Autowired
+    private SwaggerConfiguration swaggerConfiguration;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Bean
-    public Docket swaggerConfiguration() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .paths(PathSelectors.ant("/api/**"))
-                .apis(RequestHandlerSelectors.basePackage("com.gridstudentpractice.chatservice"))
-                .build()
-                .apiInfo(apiDetails());
-    }
-
-    private ApiInfo apiDetails() {
-        return new ApiInfo(
-                "Chatservice API",
-                "API for Grid Dynamics student practice",
-                "1.0",
-                "Free to use",
-                new Contact("Mantarus", "https://github.com/Mantarus", "https://github.com/Mantarus"),
-                "API License",
-                "https://github.com/Mantarus/grid-student-practice",
-                Collections.emptyList());
+    public Docket setSwaggerConfiguration() {
+        return swaggerConfiguration.setSwaggerConfiguration();
     }
 
 }
---
+++ backend/src/main/java/com/gridstudentpractice/chatservice/config/SwaggerConfiguration.java
@@ -0,0 +1,37 @@
+package com.gridstudentpractice.chatservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    public Docket setSwaggerConfiguration() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/api/**"))
+                .apis(RequestHandlerSelectors.basePackage("com.gridstudentpractice.chatservice"))
+                .build()
+                .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                "Chatservice API",
+                "API for Grid Dynamics student practice",
+                "1.0",
+                "Free to use",
+                new Contact("Mantarus", "https://github.com/Mantarus", "https://github.com/Mantarus"),
+                "API License",
+                "https://github.com/Mantarus/grid-student-practice",
+                Collections.emptyList());
+    }
+
+}
