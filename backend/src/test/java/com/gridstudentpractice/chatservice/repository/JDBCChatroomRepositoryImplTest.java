5b7dd0bbf962daef00af338c9246129322ee63ef
Вова <vovan_lipkin@mail.ru>
2020-03-23 15:19:39 +0300

    Chatroom repository tests

--- backend/src/test/java/com/gridstudentpractice/chatservice/repository/JDBCChatroomRepositoryImplTest.java
+++ backend/src/test/java/com/gridstudentpractice/chatservice/repository/JDBCChatroomRepositoryImplTest.java
@@ -1,5 +1,6 @@
 package com.gridstudentpractice.chatservice.repository;
 
+import com.gridstudentpractice.chatservice.exception.RepositoryException;
 import com.gridstudentpractice.chatservice.model.Chatroom;
 
 import org.junit.After;
@@ -12,9 +13,11 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.PostConstruct;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 
 @RunWith(SpringRunner.class)
@@ -44,26 +47,43 @@ public class JDBCChatroomRepositoryImplTest {
                                                             ");";
     private static final String dropChatroomTableQuery = "DROP TABLE chatrooms;";
     private static final String clearChatroomTableQuery = "DELETE FROM chatrooms;";
-    private static final String insertUsersQuery = "INSERT INTO chatrooms VALUES (1, 'foo1', 'pass1'), " +
-                                                                            "(2, 'foo2', 'pass2'), " +
-                                                                            "(3, 'foo3', 'pass3');";
+    private static final String insertChatroomQuery = "INSERT INTO chatrooms VALUES (1, 'chatroom', 'description1'), " +
+                                                                            "(2, 'chatroom', 'description2'), " +
+                                                                            "(3, 'chatroom3', 'description3');";
+    private static final String getChatrooms = "SELECT * FROM chatrooms ";
+    private static final String insertOneChatroomQuery = "INSERT INTO chatrooms VALUES (1, 'chatroom', 'description1')";
+    private static final String createUserChatroomTableQuery = "CREATE TABLE user_chatroom " +
+                                                            "( " +
+                                                            "user_id INT NOT NULL , " +
+                                                            "chatroom_id INT NOT NULL  " +
+                                                            ");";
+    private static final String getUserChatroom = "SELECT * FROM user_chatroom ";
 
     @Before
-    public void beforeAll() throws SQLException {
+    public void before() throws SQLException {
         Statement statement = connection.createStatement();
         statement.executeUpdate(createChatroomTableQuery);
         statement.close();
     }
 
     @After
-    public void afterAll() throws SQLException {
+    public void after() throws SQLException {
         Statement statement = connection.createStatement();
         statement.executeUpdate(dropChatroomTableQuery);
         statement.close();
     }
 
+    private Chatroom findChatroomById(int id, List<Chatroom> chatrooms) {
+        for (Chatroom chatroom : chatrooms) {
+            if (chatroom.getId() == id) {
+                return chatroom;
+            }
+        }
+        return null;
+    }
+
     @Test
-    public void createChatroom() {
+    public void createChatroom() throws SQLException {
 
         Chatroom chatroom = Chatroom.builder()
                             .description("Description1")
@@ -73,33 +93,117 @@ public class JDBCChatroomRepositoryImplTest {
 
         chatroomRepository.createChatroom(chatroom);
 
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(getChatrooms);
+        if (rs.next()) {
+            assertEquals(chatroom.getId(), rs.getInt("id"));
+            assertEquals(chatroom.getDescription(), rs.getString("description"));
+            assertEquals(chatroom.getName(), rs.getString("name"));
+
+        }
+        statement.close();
 
     }
 
     @Test
-    public void getChatroomById() {
+    public void getChatroomById() throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(insertChatroomQuery);
+
+        Chatroom chatroom = chatroomRepository.getChatroomById(3);
+
+        assertEquals(chatroom.getId(), 3);
+        assertEquals(chatroom.getDescription(), "description3");
+        assertEquals(chatroom.getName(), "chatroom3");
+
+        statement.close();
+    }
+
+    @Test
+    public void getChatroomsByName() throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(insertChatroomQuery);
+
+        List<Chatroom> chatrooms = chatroomRepository.getChatroomsByName("chatroom");
+
+        assertEquals(chatrooms.get(0).getId(), 1);
+        assertEquals(chatrooms.get(0).getName(), "chatroom");
+        assertEquals(chatrooms.get(0).getDescription(), "description1");
+
+        assertEquals(chatrooms.get(1).getId(), 2);
+        assertEquals(chatrooms.get(1).getName(), "chatroom");
+        assertEquals(chatrooms.get(1).getDescription(), "description2");
+
+        statement.close();
+    }
+
+    @Test
+    public void updateChatroom() throws SQLException {
+
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(insertOneChatroomQuery);
+        statement.close();
+
+        Chatroom chatroom = Chatroom.builder()
+                .description("updatedDescription")
+                .id(1)
+                .name("updatedChatroom")
+                .build();
+
+        chatroomRepository.updateChatroom(chatroom);
+
+        Statement statement1 = connection.createStatement();
+        ResultSet rs = statement1.executeQuery(getChatrooms);
+        if (rs.next()) {
+            assertEquals(chatroom.getId(), rs.getInt("id"));
+            assertEquals(chatroom.getDescription(), rs.getString("description"));
+            assertEquals(chatroom.getName(), rs.getString("name"));
+
+        }
+        statement1.close();
+    }
+
+    @Test
+    public void deleteChatroomById() throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(insertChatroomQuery);
+        statement.close();
+
+        chatroomRepository.deleteChatroomById(1);
+
+        Statement statement1 = connection.createStatement();
+        ResultSet rs = statement1.executeQuery(getChatrooms);
+
+        List<Chatroom> chatrooms = new ArrayList<>();
+        while (rs.next()) {
+            Chatroom chatroom = Chatroom.builder()
+                    .id(rs.getInt("id"))
+                    .name(rs.getString("name"))
+                    .description(rs.getString("description"))
+                    .build();
+            chatrooms.add(chatroom);
+        }
+        statement1.close();
+
+        assertNull(findChatroomById(1,chatrooms));
 
     }
 
     @Test
-    public void getChatroomsByName() {
+    public void addUserToChatroom() throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(createUserChatroomTableQuery);
+        statement.close();
 
+        chatroomRepository.addUserToChatroom(1,1);
+
+        Statement statement1 = connection.createStatement();
+        ResultSet rs = statement1.executeQuery(getUserChatroom);
+        if(rs.next()) {
+            assertEquals(rs.getInt("user_id"), 1);
+            assertEquals(rs.getInt("chatroom_id"), 1);
+        }
+
+        statement1.close();
     }
-
-    @Test
-    public void updateChatroom() {
-
-    }
-
-    @Test
-    public void deleteChatroomById() {
-
-    }
-
-    @Test
-    public void addUserToChatroom() {
-
-    }
-
-
 }
