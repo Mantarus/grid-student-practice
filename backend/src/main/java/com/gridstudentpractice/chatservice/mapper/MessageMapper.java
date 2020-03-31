735df9b912c8204c370c99d7b3a217481babed24
Trofim <ktrof18@gmail.com>
2020-03-31 23:02:19 +0300

    Creating ORM repository methods

--- backend/src/main/java/com/gridstudentpractice/chatservice/mapper/MessageMapper.java
+++ backend/src/main/java/com/gridstudentpractice/chatservice/mapper/MessageMapper.java
@@ -4,59 +4,26 @@ import com.gridstudentpractice.chatservice.model.*;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
-import org.mapstruct.Named;
+import org.mapstruct.NullValueCheckStrategy;
 
-@Mapper
+@Mapper(nullValueCheckStrategy = NullValueCheckStrategy.ON_IMPLICIT_CONVERSION)
 public interface MessageMapper {
 
     @Mappings({
             @Mapping(target = "id", source = "messageEntity.id"),
-            @Mapping(target = "sender", source = "messageEntity.sender", qualifiedByName = "senderDTO"),
+            @Mapping(target = "sender", source = "messageEntity.sender.login"),
             @Mapping(target = "body", source = "messageEntity.body"),
             @Mapping(target = "timestamp", source = "messageEntity.timestamp"),
-            @Mapping(target = "chatroom", source = "messageEntity.chatroom", qualifiedByName = "chatroomDTO")
+            @Mapping(target = "chatroom", source = "messageEntity.chatroom.name")
     })
     Message toDTO(MessageEntity messageEntity);
 
-    @Named("senderDTO")
-    default String entityLoginToString(UserEntity userEntity) {
-        if (userEntity != null) {
-            return userEntity.getLogin();
-        } else return null;
-    }
-
-    @Named("chatroomDTO")
-    default String entityChatroomToString(ChatroomEntity chatroomEntity) {
-        if (chatroomEntity != null) {
-            return chatroomEntity.getName();
-        } else return null;
-    }
-
     @Mappings({
             @Mapping(target = "id", source = "message.id"),
-            @Mapping(target = "sender", source = "message.sender", qualifiedByName = "sender"),
+            @Mapping(target = "sender.id", source = "message.sender"),
             @Mapping(target = "body", source = "message.body"),
             @Mapping(target = "timestamp", source = "message.timestamp"),
-            @Mapping(target = "chatroom", source = "message.chatroom", qualifiedByName = "chatroom")
+            @Mapping(target = "chatroom.id", source = "message.chatroom")
     })
     MessageEntity toEntity(Message message);
-    MessageMapperImpl
-    @Named("sender")
-    default UserEntity loginStringToEntity(String login) {
-        if (login != null) {
-            UserEntity userEntity = new UserEntity();
-            userEntity.setId(Integer.parseInt(login));
-            return userEntity;
-        } else return null;
-    }
-
-    @Named("chatroom")
-    default ChatroomEntity chatroomStringToEntity(String chatroom) {
-        if (chatroom != null) {
-            ChatroomEntity chatroomEntity = new ChatroomEntity();
-            chatroomEntity.setId(Integer.parseInt(chatroom));
-            return chatroomEntity;
-        } else return null;
-    }
-
 }
--- backend/src/main/java/com/gridstudentpractice/chatservice/service/DBMessageServiceImpl.java
+++ backend/src/main/java/com/gridstudentpractice/chatservice/service/DBMessageServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Service
  public class DBMessageServiceImpl implements MessageService {
 
-    @Qualifier("JDBCMessageRepositoryImpl")
+    @Qualifier("ORMMessageRepositoryImpl")
     @Autowired
     private MessageRepository messageRepository;
 
