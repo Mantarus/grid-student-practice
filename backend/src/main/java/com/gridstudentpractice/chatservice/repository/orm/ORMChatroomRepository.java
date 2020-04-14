185d4a9ab59bda42ef5bb0636bd9ce4a0d546f54
Вова <vovan_lipkin@mail.ru>
2020-04-14 23:36:09 +0300

    liquibase was added, but doesn't work properly

--- backend/src/main/java/com/gridstudentpractice/chatservice/repository/orm/ORMChatroomRepository.java
+++ backend/src/main/java/com/gridstudentpractice/chatservice/repository/orm/ORMChatroomRepository.java
@@ -1,7 +1,6 @@
 package com.gridstudentpractice.chatservice.repository.orm;
 
 import com.gridstudentpractice.chatservice.model.ChatroomEntity;
-import io.swagger.models.auth.In;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
