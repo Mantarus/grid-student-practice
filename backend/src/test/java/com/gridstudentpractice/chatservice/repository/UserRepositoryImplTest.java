f6b20b97364a4c3cfa0c0246ab55582808d900cc
Вова <vovan_lipkin@mail.ru>
2020-05-15 22:31:07 +0300

    Profiles fix

--- backend/src/test/java/com/gridstudentpractice/chatservice/repository/UserRepositoryImplTest.java
+++ backend/src/test/java/com/gridstudentpractice/chatservice/repository/UserRepositoryImplTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
@@ -24,6 +25,7 @@ import java.util.List;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 //@ActiveProfiles({"test","orm"})
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class UserRepositoryImplTest {
 
     @Autowired
